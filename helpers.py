28ed3b6b694d02ca4f97013da996e1d16fbe7526
Vidur Katyal <vidurkatyal95@gmail.com>
2016-04-24 13:43:59 +0530

    Render file thumbnails on import

---
+++ helpers.py
@@ -0,0 +1,190 @@
+import shutil
+import os
+import threading
+import cairo
+import math
+import time
+
+import gi
+gi.require_version('Poppler', '0.18')
+from gi.repository import Poppler
+from gi.repository import Gtk
+from gi.repository import GObject
+
+
+class PDF_Doc:
+    """Class for handling PDF documents."""
+
+    def __init__(self, _file, numfiles, tmp_dir):
+
+        self._file = os.path.abspath(_file)
+        (self.path, self.filename) = os.path.split(self._file)
+        (self.filename, self.ext) = os.path.splitext(self.filename)
+        file_prefix = 'file://'
+
+        self.filenum = numfiles + 1
+        self.mtime = os.path.getmtime(_file)
+        self.copyname = os.path.join(tmp_dir, '%02d_' % self.filenum +
+                                                  self.filename + '.pdf')
+        shutil.copy(self._file, self.copyname)
+        self.document = Poppler.Document.new_from_file(file_prefix + self.copyname, None)
+        self.numpages = self.document.get_n_pages()
+
+class PDF_Renderer(threading.Thread, GObject.GObject):
+    "Class for rendering thumbnails of pages."
+
+    def __init__(self, model, pdfqueue):
+        threading.Thread.__init__(self)
+        GObject.GObject.__init__(self)
+        self.model = model
+        self.pdfqueue = pdfqueue
+        self.quit = False
+
+    def run(self):
+        for idx, row in enumerate(self.model):
+            if self.quit:
+                return
+            if not row[1]:
+                try:
+                    nfile = row[2]
+                    npage = row[3]
+                    pdfdoc = self.pdfqueue[nfile - 1]
+                    page = pdfdoc.document.get_page(npage-1)
+                    w, h = page.get_size()
+                    thumbnail = cairo.ImageSurface(cairo.FORMAT_ARGB32,
+                                                   int(w/2.0),
+                                                   int(h/2.0))
+                    cr = cairo.Context(thumbnail)
+                    cr.scale(1.0/2.0, 1.0/2.0)
+                    page.render(cr)
+                    time.sleep(0.003)
+                    GObject.idle_add(self.emit,'update_thumbnail',
+                                     idx, thumbnail, priority=GObject.PRIORITY_LOW)
+                except Exception as e:
+                    print(e)
+
+
+
+class CellRendererImage(Gtk.CellRenderer):
+    __gproperties__ = {
+            "image": (GObject.TYPE_PYOBJECT, "Image", "Image",
+                      GObject.PARAM_READWRITE),
+            "width": (GObject.TYPE_FLOAT, "Width", "Width",
+                      0., 1.e4, 0., GObject.PARAM_READWRITE),
+            "height": (GObject.TYPE_FLOAT, "Height", "Height",
+                       0., 1.e4, 0., GObject.PARAM_READWRITE),
+            "rotation": (GObject.TYPE_INT, "Rotation", "Rotation",
+                         0, 360, 0, GObject.PARAM_READWRITE),
+            "scale": (GObject.TYPE_FLOAT, "Scale", "Scale",
+                      0.01, 100., 1., GObject.PARAM_READWRITE),
+            "cropL": (GObject.TYPE_FLOAT, "CropL", "CropL",
+                      0., 1., 0., GObject.PARAM_READWRITE),
+            "cropR": (GObject.TYPE_FLOAT, "CropR", "CropR",
+                      0., 1., 0., GObject.PARAM_READWRITE),
+            "cropT": (GObject.TYPE_FLOAT, "CropT", "CropT",
+                      0., 1., 0., GObject.PARAM_READWRITE),
+            "cropB": (GObject.TYPE_FLOAT, "CropB", "CropB",
+                      0., 1., 0., GObject.PARAM_READWRITE),
+    }
+
+    def __init__(self):
+        Gtk.CellRenderer.__init__(self)
+        self.th1 = 2. # border thickness
+        self.th2 = 3. # shadow thickness
+
+    def get_geometry(self):
+
+        rotation = int(self.rotation) % 360
+        rotation = round(rotation / 90) * 90
+        if not self.image:
+            w0 = w1 = self.width / 2.0
+            h0 = h1 = self.height / 2.0
+        else:
+            w0 = self.image.get_width()
+            h0 = self.image.get_height()
+            if rotation == 90 or rotation == 270:
+                w1, h1 = h0, w0
+            else:
+                w1, h1 = w0, h0
+
+        x = self.cropL * w1
+        y = self.cropT * h1
+
+        scale = 2.0 * self.scale
+        w2 = int(scale * (1. - self.cropL - self.cropR) * w1)
+        h2 = int(scale * (1. - self.cropT - self.cropB) * h1)
+        
+        return w0,h0,w1,h1,w2,h2,rotation
+
+    def do_set_property(self, pspec, value):
+        setattr(self, pspec.name, value)
+
+    def do_get_property(self, pspec):
+        return getattr(self, pspec.name)
+
+    def do_render(self, window, widget, background_area, cell_area, \
+                 expose_area):
+        if not self.image:
+            return
+
+        w0,h0,w1,h1,w2,h2,rotation = self.get_geometry()
+        th = int(2*self.th1+self.th2)
+        w = w2 + th
+        h = h2 + th
+
+        x = cell_area.x
+        y = cell_area.y
+        if cell_area and w > 0 and h > 0:
+            x += self.get_property('xalign') * \
+                 (cell_area.width - w - self.get_property('xpad'))
+            y += self.get_property('yalign') * \
+                 (cell_area.height - h - self.get_property('ypad'))
+
+        window.translate(x,y)
+
+        x = self.cropL * w1
+        y = self.cropT * h1
+
+        #shadow
+        window.set_source_rgb(0.5, 0.5, 0.5)
+        window.rectangle(th, th, w2, h2)
+        window.fill()
+
+        #border
+        window.set_source_rgb(0, 0, 0)
+        window.rectangle(0, 0, w2+2*self.th1, h2+2*self.th1)
+        window.fill()
+
+        #image
+        window.set_source_rgb(1, 1, 1)
+        window.rectangle(self.th1, self.th1, w2, h2)
+        window.fill_preserve()
+        window.clip()
+
+        window.translate(self.th1,self.th1)
+        scale = 2.0 * self.scale
+        window.scale(scale, scale)
+        window.translate(-x,-y)
+        if rotation > 0:
+            window.translate(w1/2,h1/2)
+            window.rotate(rotation * math.pi / 180)
+            window.translate(-w0/2,-h0/2)
+
+        window.set_source_surface(self.image)
+        window.paint()
+
+    def do_get_size(self, widget, cell_area=None):
+        x = y = 0
+        w0,h0,w1,h1,w2,h2,rotation = self.get_geometry()
+        th = int(2*self.th1+self.th2)
+        w = w2 + th
+        h = h2 + th
+
+        if cell_area and w > 0 and h > 0:
+            x = self.get_property('xalign') * \
+                (cell_area.width - w - self.get_property('xpad'))
+            y = self.get_property('yalign') * \
+                (cell_area.height - h - self.get_property('ypad'))
+        w += 2 * self.get_property('xpad')
+        h += 2 * self.get_property('ypad')
+        return int(x), int(y), w, h
--- main.py
+++ main.py
@@ -6,19 +6,40 @@ except ValueError, e:
     raise e
 
 import sys
-import os
 import mimetypes
+import tempfile
 from gi.repository import Gdk
+
+from helpers import *
+
 
 
 class PDFShuffler:
     """Class for PDFShuffler."""
+    MODEL_ROW_INTERN = 1001
+    MODEL_ROW_EXTERN = 1002
+    TEXT_URI_LIST = 1003
+    MODEL_ROW_MOTION = 1004
+    TARGETS_ICONVIEW = [Gtk.TargetEntry.new('MODEL_ROW_INTERN', Gtk.TargetFlags.SAME_WIDGET, MODEL_ROW_INTERN),
+                  Gtk.TargetEntry.new('MODEL_ROW_EXTERN', Gtk.TargetFlags.OTHER_APP, MODEL_ROW_EXTERN),
+                  Gtk.TargetEntry.new('MODEL_ROW_MOTION', 0, MODEL_ROW_MOTION)]
+    TARGETS_SW = [Gtk.TargetEntry.new('text/uri-list', 0, TEXT_URI_LIST),
+                  Gtk.TargetEntry.new('MODEL_ROW_EXTERN', Gtk.TargetFlags.OTHER_APP, MODEL_ROW_EXTERN)]
+
 
     def __init__(self):
+        # Make a temporary working directory.
+        self.tmp_dir = tempfile.mkdtemp("shuffler_working_dir")
+        os.chmod(self.tmp_dir, 0o700)
+
+
+        # Build UI from glade file and connect handlers.
         self.uiBuilder = Gtk.Builder()
         self.uiBuilder.add_from_file("GUI.glade")
         self.uiBuilder.connect_signals(self)
 
+
+        # Create the main window.
         self.window = self.uiBuilder.get_object("main_window")
         self.window.set_title("PDFShuffler")
         self.window.set_border_width(0)
@@ -29,19 +50,121 @@ class PDFShuffler:
         self.window.connect('delete-event', self.close_window)
 
 
+        # Settings for ScrolledWindow
+        self.sw = self.uiBuilder.get_object('scrolledwindow')
+        self.sw.drag_dest_set(Gtk.DestDefaults.MOTION |
+                              Gtk.DestDefaults.HIGHLIGHT |
+                              Gtk.DestDefaults.DROP |
+                              Gtk.DestDefaults.MOTION,
+                              self.TARGETS_SW,
+                              Gdk.DragAction.COPY |
+                              Gdk.DragAction.MOVE)
 
+        align = Gtk.Alignment.new(0.5, 0.5, 0, 0)
+        self.sw.add_with_viewport(align)
+
+        # Create the progress bar
+        self.progress_bar = self.uiBuilder.get_object('progressbar')
+        self.progress_bar_timeout_id = 0
+
+        # Model to hold individual pages of a pdf.
+        # (In a tabular format i.e. rows and columns)
+        # Defining columns
+        self.model = Gtk.ListStore(str,         # 0. Thumbnail caption
+                                   GObject.TYPE_PYOBJECT, # 1.Cached page image
+                                   int,         # 2. Document number
+                                   int,         # 3. Page number
+                                   float,       # 4. Scale
+                                   str,         # 5. Document filename
+                                   int,         # 6. Rotation angle
+                                   float,       # 7. Crop left
+                                   float,       # 8. Crop right
+                                   float,       # 9. Crop top
+                                   float,       # 10. Crop bottom
+                                   float,       # 11. Page width
+                                   float)       # 12. Page height
+
+        self.zoom_set(-14)
+
+        # Settings for thumbnails
+        self.iconview_col_width = 300
+        self.iconview = Gtk.IconView(self.model)
+        self.iconview.clear()
+        self.iconview.set_item_width(-1)
+
+        self.cellthumb = CellRendererImage()
+        self.iconview.pack_start(self.cellthumb, False)
+        self.iconview.set_cell_data_func(self.cellthumb, self.set_cell_data, None)
+
+
+
+        # Selection settings for iconview.
+        self.iconview.set_text_column(0)
+        self.iconview.set_selection_mode(Gtk.SelectionMode.MULTIPLE)
+        self.iconview.enable_model_drag_source(Gdk.ModifierType.BUTTON1_MASK,
+                                               self.TARGETS_ICONVIEW,
+                                               Gdk.DragAction.COPY |
+                                               Gdk.DragAction.MOVE)
+        self.iconview.enable_model_drag_dest(self.TARGETS_ICONVIEW,
+                                             Gdk.DragAction.DEFAULT)
+
+        # TODO: Connect drag and drop events to handlers
+
+
+
+
+
+        align.add(self.iconview)
+
+        # Change iconview color background
+        style_context_sw = self.sw.get_style_context()
+        color_selected = self.iconview.get_style_context().get_background_color(Gtk.StateFlags.SELECTED)
+        color_prelight = color_selected.copy()
+        color_prelight.alpha = 0.3
+        for state in (Gtk.StateFlags.NORMAL, Gtk.StateFlags.ACTIVE):
+           self.iconview.override_background_color(state, style_context_sw.get_background_color(state))
+        self.iconview.override_background_color(Gtk.StateFlags.SELECTED, color_selected)
+        self.iconview.override_background_color(Gtk.StateFlags.PRELIGHT, color_prelight)
+
+
+        # Connecting size change handler to window.
+        self.window.connect('size_allocate', self.window_resize)
         self.window.show_all()
+        self.progress_bar.hide()
 
+
+        # Variables
         self.import_directory = self.export_directory = os.getenv('HOME')
+        self.pdfqueue = []
+        self.numfiles = 0
+        self.iconview_auto_scroll_direction = 0
+        self.iconview_auto_scroll_timer = None
+
+
+        GObject.type_register(PDF_Renderer)
+        GObject.signal_new('update_thumbnail', PDF_Renderer,
+                           GObject.SignalFlags.RUN_FIRST, None,
+                           [GObject.TYPE_INT, GObject.TYPE_PYOBJECT])
+        self.rendering_thread = 0
 
 
     def close_window(self, widget, event=None, data=None):
         """Handler for closing the application."""
 
+        if os.path.isdir(self.tmp_dir):
+            shutil.rmtree(self.tmp_dir)
+
         if Gtk.main_level():
             Gtk.main_quit()
         else:
             sys.exit(0)
+
+    def window_resize(self, window, event):
+        """Handler for window resize."""
+
+        den = 10 * (self.iconview_col_width + self.iconview.get_column_spacing() * 2)
+        col_num = 9 * window.get_size()[0] / den
+        self.iconview.set_columns(col_num)
 
     def import_pdf(self, widget, data=None):
         """Handler for importing new pdf(s)."""
@@ -71,10 +194,12 @@ class PDFShuffler:
                 self.import_directory = self.export_directory = path
                 file_mimetype = mimetypes.guess_type(_file)[0]
                 if file_mimetype != 'application/pdf':
-                    errors += "File %s not supported! Hence not importing..\n" % filename
+                    errors += "File %s not supported! Hence not importing...\n" % filename
                     continue
-                print "File %s qualifies" % filename
-                # TODO: Add the file to application
+
+                rv = self.add_pdf(_file)
+                if not rv:
+                    errors += "Error import file %s...\n" % filename
 
             if errors:
                 self.error_message_dialog(errors)
@@ -117,10 +242,69 @@ class PDFShuffler:
         file_export.destroy()
 
 
+    def add_pdf(self, _file, startpage=None, endpage=None,
+                            angle=0.0, crop=[0.0, 0.0, 0.0, 0.0]):
+        """Function to add pdf to the application."""
+
+        pdfdoc = None
+        rv = False
+
+        for pdf in self.pdfqueue:
+            if os.path.isfile(pdf.filename):
+                if os.path.samefile(_file, pdf.filename):
+                    if os.path.getmtime(_file) is pdf.mtime:
+                        pdfdoc = pdf
+                        break
+
+
+        if not pdfdoc:
+            pdfdoc = PDF_Doc(_file, self.numfiles, self.tmp_dir)
+            if not pdfdoc:
+                return rv
+
+            self.numfiles = pdfdoc.filenum
+            self.pdfqueue.append(pdfdoc)
+
+
+        start = 1
+        end = pdfdoc.numpages
+        if startpage:
+            start = min(end, max(1, startpage))
+        if endpage:
+            end = max(start, min(end, endpage))
+
+
+        for page_num in range(start, end + 1):
+            caption = pdfdoc.filename + '\nPage ' + str(page_num)
+            page = pdfdoc.document.get_page(page_num-1)
+            width, height = page.get_size()
+            iter = self.model.append((caption,
+                                      None,
+                                      pdfdoc.filenum,
+                                      page_num,
+                                      self.zoom_scale,
+                                      pdfdoc._file,
+                                      angle,
+                                      crop[0],
+                                      crop[1],
+                                      crop[2],
+                                      crop[3],
+                                      width,
+                                      height
+                                    ))
+            self.update_geometry(iter)
+            rv = True
+
+        self.reset_iconview_width()
+        if rv:
+            GObject.idle_add(self.render)
+        return rv
+
 
 
     def error_message_dialog(self, msg):
         """Function to display error messages."""
+
         dialog = Gtk.MessageDialog(flags=Gtk.DialogFlags.MODAL,
                                           type=Gtk.MessageType.ERROR,
                                           parent=self.window,
@@ -131,7 +315,115 @@ class PDFShuffler:
             dialog.destroy()
 
 
+    def set_cell_data(self, column, cell, model, iter, data=None):
+        """Function to set cell data."""
+
+        cell.set_property('image', model.get_value(iter,1))
+        cell.set_property('scale', model.get_value(iter,4))
+        cell.set_property('rotation', model.get_value(iter,6))
+        cell.set_property('cropL', model.get_value(iter,7))
+        cell.set_property('cropR', model.get_value(iter,8))
+        cell.set_property('cropT', model.get_value(iter,9))
+        cell.set_property('cropB', model.get_value(iter,10))
+        cell.set_property('width', model.get_value(iter,11))
+        cell.set_property('height', model.get_value(iter,12))
+
+
+
+    def reset_iconview_width(self, renderer=None):
+        """Function to reconfigure the width of the iconview columns."""
+
+        if not self.model.get_iter_first(): # Check if the model is empty
+            return
+
+        max_w = 10 + int( max(row[4]*row[11]*(1.0-row[7]-row[8]) for row in self.model) )
+        if max_w != self.iconview_col_width:
+            self.iconview_col_width = max_w
+            self.iconview.set_item_width(-1)
+
+            self.window_resize(self.window, None)
+
+    def render(self):
+        """Function to render the thumbnails."""
+
+        if self.rendering_thread:
+            self.rendering_thread.quit = True
+            self.rendering_thread.join()
+
+        self.rendering_thread = PDF_Renderer(self.model, self.pdfqueue)
+        self.rendering_thread.connect('update_thumbnail', self.update_thumbnail)
+        self.rendering_thread.start()
+
+        if self.progress_bar_timeout_id:
+            GObject.source_remove(self.progress_bar_timeout_id)
+        self.progress_bar_timout_id = GObject.timeout_add(50, self.progress_bar_timeout)
+
+    def update_geometry(self, iter):
+        """Function to recompute the width and height of rotated pages."""
+
+        if not self.model.iter_is_valid(iter):
+            return
+
+        filenum, page_num, rotation = self.model.get(iter, 2, 3, 6)
+        crop = self.model.get(iter, 7, 8, 9, 10)
+        page = self.pdfqueue[filenum-1].document.get_page(page_num-1)
+        w0, h0 = page.get_size()
+
+        rotation = int(rotation) % 360
+        rotation = round(rotation / 90) * 90
+        if rotation == 90 or rotation == 270:
+            w1, h1 = h0, w0
+        else:
+            w1, h1 = w0, h0
+
+        self.model.set(iter, 11, w1, 12, h1)
+
+
+    def progress_bar_timeout(self):
+        """Function for progress bar."""
+
+        cnt_finished = 0
+        cnt_all = 0
+        for row in self.model:
+            cnt_all += 1
+            if row[1]:
+                cnt_finished += 1
+        fraction = float(cnt_finished)/float(cnt_all)
+
+        self.progress_bar.set_fraction(fraction)
+        self.progress_bar.set_text('Rendering thumbnails... [%(i1)s/%(i2)s]'
+                                   % {'i1' : cnt_finished, 'i2' : cnt_all})
+        if fraction >= 0.999:
+            self.progress_bar.hide()
+            return False
+        elif not self.progress_bar.get_visible():
+            self.progress_bar.show()
+
+        return True
+
+    def update_thumbnail(self, object, num, thumbnail):
+        """Function to update the thumbnail."""
+
+        row = self.model[num]
+        row[1] = thumbnail
+        row[4] = self.zoom_scale
+
+    def zoom_set(self, level):
+        """Function to set the zoom level."""
+
+        self.zoom_level = max(min(level, 5), -24)
+        self.zoom_scale = 1.1 ** self.zoom_level
+        for row in self.model:
+            row[4] = self.zoom_scale
+
+    def zoom_change(self, step=5):
+        """Function to modify the zoom level."""
+
+        self.zoom_set(self.zoom_level + step)
+
+
 def main():
+    GObject.threads_init()
     PDFShuffler()
     Gtk.main()
 
